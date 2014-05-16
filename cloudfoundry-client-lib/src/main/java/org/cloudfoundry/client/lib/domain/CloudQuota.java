71cbaadcfbc7af41af9ced525ce8ae1595c20d9d
resouer <resouer@163.com>
2014-05-16 22:32:56 +0800

    Add attributes to Quota, need to parse entity next.

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/domain/CloudQuota.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/domain/CloudQuota.java
@@ -1,6 +1,38 @@
+/*
+ * Copyright 2009-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.cloudfoundry.client.lib.domain;
 
+/**
+ * 
+ * @author Harry Zhang
+ *
+ */
 public class CloudQuota extends CloudEntity {
 
-	//TODO what's missing beside meta?
+    private boolean non_basic_services_allowed = false;
+
+    public CloudQuota(Meta meta, String name,
+            boolean non_basic_services_allowed, int total_services,
+            int total_routes, long memory_limit) {
+        super(meta, name);
+        this.non_basic_services_allowed = non_basic_services_allowed;
+
+    }
+    public boolean isNon_basic_services_allowed() {
+        return non_basic_services_allowed;
+    }
+
 }
